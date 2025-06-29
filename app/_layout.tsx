0d0aca6d6f4ea3f70e9b731e333e1efc9b549490
MdThoratIslam <150887067+MdThoratIslam@users.noreply.github.com>
2025-06-29 14:55:43 +0600

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
 import { Stack } from "expo-router";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { AuthProvider } from "../contexts/AuthContext";
+import { AuthProvider } from "../contexts/AuthContext"
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
     const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
       unsavedChangesWarning: false,
     });
